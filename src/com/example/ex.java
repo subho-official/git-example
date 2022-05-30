68a2f95b368ef6befdd3e1e6981b8ac15a0194e0
Subhojit Paul <subhojit.official197@gmail.com>
2022-05-30 20:38:05 +0530

    SECOND COMMIT

--- src/com/example/ex.java
+++ src/com/example/ex.java
@@ -4,6 +4,8 @@ public class ex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("I AM AWESOME");
+		System.out.println("SATTWIK OUR SAVIOUR");
 
 	}
 
